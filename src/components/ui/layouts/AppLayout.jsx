f7b21754abc4bbb3b709e642e970b75f35c04d56
Ervigio <ejadd_75@hotmail.com>
2024-02-24 14:26:55 +0000

    Añadir componente AppLayaout

---
+++ src/components/ui/layouts/AppLayout.jsx
@@ -0,0 +1,17 @@
+import { Header, Footer } from "../../../components";
+
+import PropTypes from "prop-types";
+
+export default function AppLayout({ children }) {
+  return (
+    <>
+      <Header />
+      <main className="min-h-[85vh]">{children}</main>
+      <Footer />
+    </>
+  );
+}
+
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};

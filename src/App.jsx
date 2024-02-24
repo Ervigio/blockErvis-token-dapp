27bd0d0fd012bc61e284a8b5b664a0fa80ebd0f4
Ervigio <ejadd_75@hotmail.com>
2024-02-24 14:28:12 +0000

    Actualizar componente App con AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
+import { AppLayout } from "./components/ui/layouts";
+
 function App() {
-   return (
-   <h1 className="text-3xl font-bold underline">
-    Hello world!   
-   <div className="bg-blue-200 flex justify-between">
-    <div className="bg-red-100 w-fit">Soy el hijo</div>
-    <div className="bg-red-100 w-fit">Soy el hijo</div>
-    <div className="bg-red-100 w-fit">Soy el hijo</div>
-   </div>
-   </h1>)
+  return (
+    <AppLayout>
+      <h1 className="text-3xl font-bold underline">Hellow World</h1>
+    </AppLayout>
+  );
 }
 
-export default App
+export default App;

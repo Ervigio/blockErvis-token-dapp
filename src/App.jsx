65a5e21787b5749b119ea6d61559ff0622eba64c
Ervigio <ejadd_75@hotmail.com>
2024-02-27 19:04:31 +0000

    Actualizar App y añair configuración de wagmi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { ConnectKitProvider } from "connectkit";
+import { WagmiConfig } from "wagmi";
 import { AppLayout } from "./components/ui/layouts";
-import {Home} from "./pages"
+import { config } from "./components/config/wagmi";
+import { Home } from "./pages";
 
 function App() {
   return (
-    <AppLayout>
-      <Home />
-    </AppLayout>
+    <WagmiConfig config={config}>
+      <ConnectKitProvider mode="light">
+        <AppLayout>
+          <Home />
+        </AppLayout>
+      </ConnectKitProvider>
+    </WagmiConfig>
   );
 }
 

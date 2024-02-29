3abaa95d5f4ba58072770edcddbfe29169af5880
Ervigio <ejadd_75@hotmail.com>
2024-02-29 00:08:24 +0000

    Actualizar componente Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,39 @@
+import { useAccount } from "wagmi";
+import { Owner, TokenInfo, TokenTransferForm } from "../components";
+import {
+  ErrorInfo,
+  LoadingSpinner,
+  Button,
+  TextInput,
+  Tittle,
+} from "../components/ui";
 export default function Home() {
-    return(
-        <div>
-            <h1 className="text-3xl font-bold underline">Hello World</h1>
-        </div>
-    )
+  const { address, isConnecting, isDisconnected } = useAccount();
+
+  if (isConnecting) return <div>Connecting...</div>;
+  if (isDisconnected) return <div>Disconnected</div>;
+
+  return (
+    <div className="flex flex-col gap-6">
+      <div>Connected Wallet: {address}</div>
+      <LoadingSpinner className="h-10 w-10" />
+      <div>
+        <Button>Comprar Tokens</Button>
+      </div>
+
+      <ErrorInfo message="Error: Internal Server Error" />
+
+      <div>
+        <TextInput />
+      </div>
+
+      <Tittle>Éste es un título de muestra</Tittle>
+
+      <Owner />
+
+      <TokenInfo />
+      
+      <TokenTransferForm />
+    </div>
+  );
 }

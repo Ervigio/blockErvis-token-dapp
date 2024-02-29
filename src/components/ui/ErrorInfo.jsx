3166d06466a1403cd44c284cd73c5121df8ed473
Ervigio <ejadd_75@hotmail.com>
2024-02-29 00:01:03 +0000

    Añadir componenete ErrorInfo

---
+++ src/components/ui/ErrorInfo.jsx
@@ -0,0 +1,14 @@
+import { PropTypes } from "prop-types";
+import { MdInfoOutline } from "react-icons/md";
+export default function ErrorInfo({ message }) {
+  return (
+    <div className="flex items-center gap-2 bg-red-200 p-2 w-fit rounded-lg">
+      <MdInfoOutline className="text-red-500" />
+      <p className="text-xs text-red-500">{message}</p>
+    </div>
+  );
+}
+
+ErrorInfo.propTypes = {
+  message: PropTypes.string,
+};

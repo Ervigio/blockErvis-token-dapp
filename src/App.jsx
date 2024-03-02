e40f53a51e289ea2698027762c9a7fbdf517f094
Ervigio <ejadd_75@hotmail.com>
2024-03-02 02:42:36 +0000

    Instalar y confirgurar react-hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { WagmiConfig } from "wagmi";
 import { AppLayout } from "./components/ui/layouts";
 import { config } from "./components/config/wagmi";
 import { Home } from "./pages";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <WagmiConfig config={config}>
+      <Toaster position="bottom-right"/>
       <ConnectKitProvider mode="light">
         <AppLayout>
           <Home />

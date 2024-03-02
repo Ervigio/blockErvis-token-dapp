f26f0e8d13bece665979c262d763f4c348073782
Ervigio <ejadd_75@hotmail.com>
2024-03-02 02:39:58 +0000

    Actualizar índice components

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Owner } from "./Owner";
 export { default as TokenInfo } from "./TokenInfo";
+export { default as TokenInfo_con_Map } from "./TokenInfo_con_Map";
 export { default as TokenTransferForm } from "./TokenTransferForm";
